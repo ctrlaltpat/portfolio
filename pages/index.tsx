369424ee773384e8b8453df8cde676dee67a2ad7
Patrick Pilgrim <rick@Zods-CmdCntr.local>
2022-06-05 22:26:01 +0100

    Home Page: create keyboard key elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import type { NextPage } from 'next'
+import { Box, Button } from '@chakra-ui/react';
+import type { NextPage } from 'next';
 // import Head from 'next/head'
 // import Image from 'next/image'
+
+const KeyButton = (text: string) => (
+  <button className='key-button'>{text}</button>
+);
 
 const Home: NextPage = () => {
   return (
     <div className={'hello'}>
-      this should be a page
+      <Box>
+        {KeyButton('Ctrl')}
+        {KeyButton('Alt')}
+        {KeyButton('Pat')}
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
