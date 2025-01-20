a1268e496720ad43b424561d7e807ed30c6ecdc8
Patrick Pilgrim <rick@Zods-CmdCntr.local>
2025-01-20 09:18:18 +0000

    chore: minor update to readme and content

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,4 +1,4 @@
-export const intro = ["Hi! I'm Patrick, a passionate full-stack developer based in London with a deep love for creating beautiful and functional web experiences.", "My journey in tech started with an interest in creating a multiplayer game in Unity. Over time, I've honed my skills in web development, and worked on a range of projects, from simple landing pages to complex web applications.", "When I'm not coding, you can find me rollerskating through London's streets, gaming, or experimenting with new technologies. I'm always excited to collaborate on interesting projects and learn new things."]
+export const intro = ["Hi! I'm Patrick, a passionate software engineer based in London with a deep love for creating beautiful and functional web experiences.", "My journey in tech started with an interest in creating a multiplayer game in Unity. Over time, I've honed my skills in web development, and worked on a range of projects, from simple landing pages to complex web applications.", "When I'm not coding, you can find me rollerskating through London's streets, gaming, or experimenting with new technologies. I'm always excited to collaborate on interesting projects and learn new things."]
 
 export const experience = [
   {
@@ -27,7 +27,7 @@ export const experience = [
     title: 'Web Developer',
     company: 'Web Agency',
     tenure: '2017 - 2018',
-    description: 'Built and maintained multiple accessible, responsive websites using PHP,HTML, CSS, and JavaScript.',
+    description: 'Built and maintained multiple accessible, responsive websites using PHP, HTML, CSS, and JavaScript.',
     achievements: [
       'Produced the intial UI for a brand launch, resulting in initial sales of over £80,000',
       'Successfully built 10+ pixel-perfect websites from bespoke designs',
