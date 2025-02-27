631772c14dfb29c08c72ee2abdfa900116584f46
Patrick <ctrlaltpat@gmail.com>
2025-02-27 23:50:52 +0000

    update techExp import; TODO find something for home page :(

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-// import Polyline from '@/components/ui/polyline';
-import { TechExp } from '@/components/ui/techExp';
-// import Image from 'next/image';
-// import styles from './styles/page.module.css';
-// import { cx } from '@/utils/styles';
+import TechExp from '@/components/ui/techExp';
 
 export default function Home() {
   return (
     <div className={""}>
-      {/* <Polyline/> */}
       <TechExp/>
-      
     </div>
   );
 }
