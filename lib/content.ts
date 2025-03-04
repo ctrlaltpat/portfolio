76403e10fb465de05990bc9724ca2db423576666
Patrick <ctrlaltpat@gmail.com>
2025-03-04 23:45:07 +0000

    minor content update

--- lib/content.ts
+++ lib/content.ts
@@ -2,44 +2,44 @@ export const intro = [
   "Hi!👋🏾 I'm Patrick, a passionate software engineer based in London with a deep love for creating beautiful and functional web experiences.",
   "My journey in tech started with an interest in creating a multiplayer game in Unity. Over time, I've honed my skills in web development, and worked on a range of projects, from simple landing pages to complex web applications.",
   "When I'm not coding, you can find me rollerskating through London's streets, gaming, or experimenting with new technologies. I'm always excited to collaborate on interesting projects and learn new things.",
-  "This is my portfolio to showcase my projects and a little about my life."
+  "This is my portfolio to showcase my projects and a little about my life.",
 ];
 
 export const experience = [
   {
     title: "Developer",
-    company: "Multimedia Company",
+    company: "Sky",
     tenure: "2022 - 2024",
     description:
       "Maintained thousands of templates using Typescipt, MJML and CSS",
     work: [
-      "Reduced visual regression testing time by 50%",
-      "Managed CI/CD Pipelines using Concourse and various scripts",
-      "Increased test coverage by 20-30%",
+      "Reduced visual regression testing time by 50%, reducing the time to production deployment",
+      "Managed and improved CI/CD Pipelines using Concourse and various scripts, enhancing developer experience",
+      "Increased test coverage by 20-30%, leading to fewer bugs and more confidence in releases",
     ],
   },
   {
     title: "Software Engineer",
-    company: "Networking Equipment and Services Company",
+    company: "Cisco",
     tenure: "2019 - 2022",
     description:
       "Developed and maintained multiple client projects using various technologies including React, Redux, TypeScript, and AngularJS.",
     work: [
-      "Collaborated with cross-functional teams to deliver high-quality software",
-      "Integrated various third-party APIs and services",
+      "Collaborated with cross-functional teams to deliver high-quality software, enhancing customer and vendor experiences",
+      "Integrated various third-party APIs and services, improving data availability and maintaining security",
       "Co-facilitated interviews for successful new hires - Software Engineer, Product Owner",
     ],
   },
   {
     title: "Web Developer",
-    company: "Web Agency",
+    company: "Absowebly",
     tenure: "2017 - 2018",
     description:
       "Built and maintained multiple accessible, responsive websites using PHP, HTML, CSS, and JavaScript.",
     work: [
       "Produced the intial UI for a brand launch, resulting in initial sales of over £80,000",
-      "Successfully built 10+ pixel-perfect websites from bespoke designs",
-      "Improved the accessibility score of websites by 20%",
+      "Successfully built 10+ pixel-perfect websites from bespoke in-house designs",
+      "Improved the accessibility score of websites by 20%, complying with National Disability Authority guidelines",
     ],
   },
 ];
