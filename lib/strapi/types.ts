31722123be408dbb0aa0d55912a18a1b82cda12e
Patrick <ctrlaltpat@gmail.com>
2025-03-10 17:34:34 +0000

    update strapi lib and switch to authenticated calls

--- lib/strapi/types.ts
+++ lib/strapi/types.ts
@@ -1,18 +1,3 @@
-type StrapiItem = {
-  id: string;
-  documentId: string;
-  description?: string;
-  createdAt: string;
-  updatedAt: string;
-};
-
-export interface MediaData {
-  url: string;
-  ext: string;
-  alternativeText?: string;
-  caption?: string;
-}
-
 export type Tag = { id?: string; title: string; documentId?: string };
 
 type content = [
@@ -23,24 +8,42 @@ type content = [
 
 type MediaType = "note" | "snippet" | "image" | "video";
 
-export interface Article extends StrapiItem {
+export interface MediaData {
+  url: string;
+  ext: string;
+  alternativeText?: string;
+  caption?: string;
+}
+
+export interface StrapiItem {
+  id: string;
   title: string;
+  documentId: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+  tags: Tag[];
+}
+
+export interface StrapiMeta {
+  pagination: {
+    page: number;
+    pageSize: number;
+    pageCount: number;
+    total: number;
+  };
+}
+
+export interface Article extends StrapiItem {
   slug: string;
   blocks: content;
   category?: {
     name: string;
   };
   cover: MediaData;
-  tags: Tag[];
-}
-
-export interface ArticlesResponse {
-  data: Article[];
 }
 
 export interface MediaItem extends StrapiItem {
-  title: string;
-  tags: Tag[];
   type: MediaType;
   content?: string;
   image?: MediaData;
@@ -48,20 +51,15 @@ export interface MediaItem extends StrapiItem {
   source?: string;
 }
 
-export interface MediaItemsResponse {
-  data: MediaItem[];
-}
-
 export interface Project extends StrapiItem {
-  title: string;
   cover: MediaData;
   demo: MediaData;
   demoLink: string;
   repo: string;
   stage: string;
-  tags: Tag[];
 }
 
-export interface ProjectsResponse {
-  data: Project[];
+export interface StrapiResponse<T extends StrapiItem> extends Response {
+  data: T[];
+  meta: StrapiMeta;
 }
